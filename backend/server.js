5a6885781e3450cd9eabdd47a443afd999ec00c5
nuha-riwaz <riwaznuha@gmail.com>
2026-04-24 00:23:34 +0530

    frontend and backend file setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const allowedOrigins = [
+    "https://my-react-router-app.vercel.app",
+    "http://localhost:5173",
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if(!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+        },
+        optionsSuccessStatus: 200,  
+};
+app.use(cors(corsOptions));
+app.use(express.json());
